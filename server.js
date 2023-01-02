046345c48e1333d78ad29f5ccd8b1e0185336027
Rohit Sharma <rohitgargi0293@gmail.com>
2023-01-02 14:44:56 +0530

    Updating serverjs

--- server.js
+++ server.js
@@ -8,21 +8,28 @@ const {
 } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
+  const headers ={
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Methods':'OPTIONS, POST, GET, PUT',
+    'Access-Control-Max-Age':2592000, //30 days
+    'Content-Type':'application/json'
+  }
+
   if (req.url === '/api/products' && req.method === 'GET') {
-    getProducts(req, res);
+    getProducts(req, res, headers );
   } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'GET') {
     const id = req.url.split('/')[3];
-    getProduct(req, res, id);
+    getProduct(req, res, id, headers);
   } else if (req.url === '/api/products' && req.method === 'POST') {
-    createProduct(req, res);
+    createProduct(req, res, headers);
   } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
     const id = req.url.split('/')[3];
-    updateProduct(req, res, id);
+    updateProduct(req, res, id, headers);
   } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
     const id = req.url.split('/')[3];
-    deleteProduct(req, res, id);
+    deleteProduct(req, res, id, headers);
   } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.writeHead(404, headers);
     res.end(
       JSON.stringify({
         message: 'Route Not Found: Please use the api/products endpoint',
